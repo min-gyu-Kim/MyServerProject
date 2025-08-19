aace9c59e4045c0b03f1cd56a8c2d7e8a00a73fa
mgkim1414 <rlaalsrb1414@naver.com>
2025-08-20 02:01:16 +0900

    프로젝트 설정 추가 - clang-tidy 추가 및 개인 설정 - clang-format 추가 및 개인 설정 - gsl 의존성 추가 - 테스트 클라이언트 코드 작성 - preset에 패키징 정보 수정

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# MyServerProject
+# pre-commit 규칙
+아래 git 설정 적용
+``` bash
+git config core.hooksPath .githooks
+```
--- src/lib-core/src/core.cpp
+++ src/lib-core/src/core.cpp
@@ -1,73 +1,77 @@
-#include <stdexcept>
-#include <fmt/core.h>
-#include <arpa/inet.h>
 #include "libcore/core.hpp"
+#include <arpa/inet.h>
+#include <fmt/core.h>
+#include <gsl/gsl>
+#include <stdexcept>
 
 namespace core {
-namespace{
+namespace {
 
-enum class eIOType{
-    ACCEPT,
-    CONNECT,
-    RECV,
-    SEND
+enum class eIOType
+{
+    Accept,
+    Connect,
+    Recv,
+    Send
 };
 
-struct IORequest {
-    eIOType type;
-    int fd; // File descriptor
-    void* buffer; // Buffer for data
-    size_t length; // Length of data
+struct IORequest
+{
+    eIOType mType;
+    int mFd;        // File descriptor
+    void *mBuffer;  // Buffer for data
+    size_t mLength; // Length of data
 };
 
 } // anonymous namespace
 
-
-Scheduler::Scheduler()
+Scheduler::Scheduler() : m_ring{}
 {
-    int result = io_uring_queue_init(256, &m_ring, 0);
-    if(result < 0)
-    {
-        throw std::runtime_error(fmt::format("Failed to initialize io_uring: {}", strerror(-result)));
+    const int QUEUE_SIZE = 256;
+    int result = io_uring_queue_init(QUEUE_SIZE, &m_ring, 0);
+    if (result < 0) {
+        throw std::runtime_error(fmt::format(
+            "Failed to initialize io_uring: {}", strerror(-result)));
     }
 
     m_listenFd = socket(AF_INET, SOCK_STREAM, 0);
-    if(m_listenFd < 0)
-    {
-        throw std::runtime_error(fmt::format("Failed to create socket: {}", strerror(errno)));
+    if (m_listenFd < 0) {
+        throw std::runtime_error(
+            fmt::format("Failed to create socket: {}", strerror(errno)));
     }
 
     sockaddr_in listenAddr{};
     listenAddr.sin_family = AF_INET;
     listenAddr.sin_addr.s_addr = INADDR_ANY;
-    listenAddr.sin_port = htons(8888);
+    const ushort PORT = 8888;
+    listenAddr.sin_port = htons(PORT);
 
-    if(bind(m_listenFd, (struct sockaddr*)&listenAddr, sizeof(listenAddr)) < 0)
-    {
-        throw std::runtime_error(fmt::format("Failed to bind socket: {}", strerror(errno)));
+    if (bind(m_listenFd, reinterpret_cast<struct sockaddr *>(&listenAddr),
+             sizeof(listenAddr)) < 0) {
+        throw std::runtime_error(
+            fmt::format("Failed to bind socket: {}", strerror(errno)));
     }
 
-    if(listen(m_listenFd, SOMAXCONN) < 0)
-    {
-        throw std::runtime_error(fmt::format("Failed to listen on socket: {}", strerror(errno)));
+    if (listen(m_listenFd, SOMAXCONN) < 0) {
+        throw std::runtime_error(
+            fmt::format("Failed to listen on socket: {}", strerror(errno)));
     }
 
-    io_uring_sqe* sqe = io_uring_get_sqe(&m_ring);
-    if(!sqe)
-    {
-        throw std::runtime_error(fmt::format("Failed to get submission queue entry: {}", strerror(errno)));
+    io_uring_sqe *sqe = io_uring_get_sqe(&m_ring);
+    if (nullptr == sqe) {
+        throw std::runtime_error(fmt::format(
+            "Failed to get submission queue entry: {}", strerror(errno)));
     }
 
-    sockaddr_in* addrPtr = new sockaddr_in;
-    IORequest* request = new IORequest{
-        eIOType::ACCEPT,
-        m_listenFd,
-        addrPtr,
-        sizeof(sockaddr_in)
-    };
-    sqe->user_data = (unsigned long long)request;
+    gsl::owner<sockaddr_in *> addrPtr = new sockaddr_in;
+    gsl::owner<IORequest *> request = new IORequest{
+        eIOType::Accept, m_listenFd, addrPtr, sizeof(sockaddr_in)};
 
-    io_uring_prep_accept(sqe, m_listenFd, (struct sockaddr*)addrPtr, (socklen_t*)&request->length, 0);
+    sqe->user_data = reinterpret_cast<unsigned long long>(request);
+
+    io_uring_prep_accept(sqe, m_listenFd,
+                         reinterpret_cast<struct sockaddr *>(addrPtr),
+                         reinterpret_cast<socklen_t *>(&request->mLength), 0);
 }
 
 Scheduler::~Scheduler()
@@ -77,60 +81,61 @@ Scheduler::~Scheduler()
 
 void Scheduler::Run()
 {
-    while(true)
-    {
-        io_uring_cqe* cqes[32];
-        __kernel_timespec ts = {1, 0}; // No timeout
-        int result = io_uring_wait_cqes(&m_ring, cqes, 32, &ts, nullptr);
-        if(result < 0)
-        {
-            if(-ETIME == result)
-            {
+    const int MAX_CQES = 32;
+    while (true) {
+        std::array<io_uring_cqe *, MAX_CQES> cqes = {};
+        __kernel_timespec timespec = {1, 0}; // No timeout
+        int result = io_uring_wait_cqes(&m_ring, cqes.data(), MAX_CQES,
+                                        &timespec, nullptr);
+        if (result < 0) {
+            if (-ETIME == result) {
                 // Handle timeout case
                 continue;
             }
-            throw std::runtime_error(fmt::format("Failed to wait for completion: {}", strerror(-result)));
+            throw std::runtime_error(fmt::format(
+                "Failed to wait for completion: {}", strerror(-result)));
         }
 
-        for(int i = 0; i < result; ++i)
-        {
-            io_uring_cqe* cqe = cqes[i];
+        for (int i = 0; i < result; ++i) {
+            io_uring_cqe *cqe = cqes.at(i);
 
-            if(cqe->res < 0)
-            {
-                throw std::runtime_error(fmt::format("IO submission failed: {}", strerror(-cqe->res)));
+            if (cqe->res < 0) {
+                throw std::runtime_error(fmt::format("IO submission failed: {}",
+                                                     strerror(-cqe->res)));
             }
 
-            IORequest* request = (IORequest*)cqe->user_data;
-            switch (request->type)
-            {
-            case eIOType::ACCEPT:
+            IORequest *request =
+                reinterpret_cast<IORequest *>(io_uring_cqe_get_data(cqe));
+            switch (request->mType) {
+            case eIOType::Accept:
             {
                 /* code */
-                OnAccept(request->fd, request->buffer, request->length);
+                OnAccept(request->mFd, request->mBuffer, request->mLength);
 
-                io_uring_sqe* sqe = io_uring_get_sqe(&m_ring);
-                if(nullptr == sqe)
-                {
+                io_uring_sqe *sqe = io_uring_get_sqe(&m_ring);
+                if (nullptr == sqe) {
                     io_uring_submit(&m_ring);
                     sqe = io_uring_get_sqe(&m_ring);
                 }
-                sqe->user_data = (unsigned long long)request;
-                io_uring_prep_accept(sqe, request->fd, (sockaddr*)request->buffer, (socklen_t*)&request->length, 0);
-            }
-                break;
-            case eIOType::CONNECT:
+                sqe->user_data = reinterpret_cast<unsigned long long>(request);
+                io_uring_prep_accept(
+                    sqe, request->mFd,
+                    reinterpret_cast<sockaddr *>(request->mBuffer),
+                    reinterpret_cast<socklen_t *>(&request->mLength), 0);
+            } break;
+            case eIOType::Connect:
                 /* code */
                 break;
-            case eIOType::RECV:
+            case eIOType::Recv:
                 /* code */
                 OnRecv(request, cqe->res);
                 break;
-            case eIOType::SEND:
+            case eIOType::Send:
                 /* code */
                 break;
             default:
-                throw std::runtime_error(fmt::format("Unknown IO type: {}", static_cast<int>(request->type)));
+                throw std::runtime_error(fmt::format(
+                    "Unknown IO type: {}", static_cast<int>(request->mType)));
                 break;
             }
 
@@ -142,68 +147,70 @@ void Scheduler::Run()
     }
 }
 
-void Scheduler::OnAccept(int clientFd, void* buffer, size_t length)
+void Scheduler::OnAccept(int clientFd, void *buffer, size_t length)
 {
     (void)length; // Unused in this context
     // Handle the accept event
-    sockaddr_in* addrPtr = (sockaddr_in*)buffer;
-    char ip[INET_ADDRSTRLEN];
-    if(nullptr == inet_ntop(AF_INET, &addrPtr->sin_addr, ip, INET_ADDRSTRLEN))
-    {
-        throw std::runtime_error(fmt::format("Failed to convert IP address: {}", strerror(errno)));
+    sockaddr_in *addrPtr = reinterpret_cast<sockaddr_in *>(buffer);
+    std::array<char, INET_ADDRSTRLEN> ipAddress = {};
+    if (nullptr == inet_ntop(AF_INET, &addrPtr->sin_addr, ipAddress.data(),
+                             INET_ADDRSTRLEN)) {
+        throw std::runtime_error(
+            fmt::format("Failed to convert IP address: {}", strerror(errno)));
     }
     unsigned short port = ntohs(addrPtr->sin_port);
 
-    fmt::print("Accepted connection from {}:{}", ip, port);
+    fmt::print("Accepted connection from {}:{}", ipAddress.data(), port);
 
-    char* recvBuffer = new char[1500];
-    IORequest* request = new IORequest{
-        eIOType::RECV,
-        clientFd,
-        recvBuffer,
-        1500
-    };
+    const int RECV_BUFFER_SIZE = 1500;
+    // NOLINTBEGIN(cppcoreguidelines-owning-memory)
+    char *recvBuffer = new char[RECV_BUFFER_SIZE];
+    gsl::owner<IORequest *> request =
+        new IORequest{eIOType::Recv, clientFd, recvBuffer, RECV_BUFFER_SIZE};
+    // NOLINTEND(cppcoreguidelines-owning-memory)
 
-    io_uring_sqe* sqe = io_uring_get_sqe(&m_ring);
-    if(nullptr == sqe)
-    {
+    io_uring_sqe *sqe = io_uring_get_sqe(&m_ring);
+    if (nullptr == sqe) {
         io_uring_submit(&m_ring);
         sqe = io_uring_get_sqe(&m_ring);
     }
-    sqe->user_data = (unsigned long long)request;
-    io_uring_prep_recv(sqe, clientFd, recvBuffer, 1500, 0);
- }
+    sqe->user_data = reinterpret_cast<unsigned long long>(request);
+    io_uring_prep_recv(sqe, clientFd, recvBuffer, RECV_BUFFER_SIZE, 0);
+}
 
-void Scheduler::OnRecv(void* request, int transferred)
+void Scheduler::OnRecv(void *request, int transferred)
 {
     // Handle the receive event
-    IORequest* req = static_cast<IORequest*>(request);
+    IORequest *req = static_cast<IORequest *>(request);
 
-    if(transferred <= 0)
-    {
-        fmt::print("Client {} disconnected or error occurred: {}", req->fd, transferred);
-        delete[] static_cast<char*>(req->buffer);
+    if (transferred <= 0) {
+        fmt::print("Client {} disconnected or error occurred: {}", req->mFd,
+                   transferred);
+        // NOLINTBEGIN(cppcoreguidelines-owning-memory)
+        delete[] static_cast<char *>(req->mBuffer);
         delete req;
+        // NOLINTEND(cppcoreguidelines-owning-memory)
         return;
     }
 
-    char* data = static_cast<char*>(req->buffer);
-    fmt::print("Received data from client {}: {}", req->fd, std::string(data, transferred));
+    char *data = static_cast<char *>(req->mBuffer);
+    fmt::print("Received data from client {}: {}", req->mFd,
+               std::string(data, transferred));
 
-    short* packetSize = (short*)data;
-    if(transferred != *packetSize)
-    {
-        fmt::print("Received incomplete packet from client {}: expected {}, got {}", req->fd, *packetSize, transferred);
+    short *packetSize = reinterpret_cast<short *>(data);
+    if (transferred != *packetSize) {
+        fmt::print(
+            "Received incomplete packet from client {}: expected {}, got {}",
+            req->mFd, *packetSize, transferred);
     }
 
-    io_uring_sqe* sqe = io_uring_get_sqe(&m_ring);
-    if(nullptr == sqe)
-    {
+    io_uring_sqe *sqe = io_uring_get_sqe(&m_ring);
+    if (nullptr == sqe) {
         io_uring_submit(&m_ring);
         sqe = io_uring_get_sqe(&m_ring);
     }
-    sqe->user_data = (unsigned long long)req;
-    io_uring_prep_recv(sqe, req->fd, req->buffer, req->length, 0);
+    sqe->user_data = reinterpret_cast<unsigned long long>(req);
+    io_uring_prep_recv(sqe, req->mFd, req->mBuffer, req->mLength, 0);
 }
 
 } // namespace core
